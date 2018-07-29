759286a3cbf3cba1cd7757ed03b040040e2f6b50
massa <massafranck@gmail.com>
2018-07-29 14:05:57 +0200

    initial

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$number1 = 0;
+$number2 = 10;
+ ?>
+ <!DOCTYPE html>
+ <html lang="fr">
+   <head>
+     <meta charset="utf-8" />
+     <title>Exercice2</title>
+   </head>
+   <body>
+<p>
+<?php
+//Incrémenter la première variable tant qu'elle n'est pas supérieure à 20
+while($number1 <= 20){
+  echo $number1 * $number2 .'<br/>';
+  $number1++;
+}
+ ?>
+</p>
+   </body>
+ </html>
